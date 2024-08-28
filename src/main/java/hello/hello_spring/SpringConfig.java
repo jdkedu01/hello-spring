32ec44f5cfcefd56083cf948d5a0826f2bc1bb80
Guman <jdkedu01@gmail.com>
2024-08-28 22:03:51 +0900

    Step6.V1

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -24,8 +24,6 @@ public class SpringConfig {
         return new MemberService(memberRepository());
     }
 
-
-
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
